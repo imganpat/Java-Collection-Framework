f6e09e61610f96d97c3854b96f48d8778a666e7c
Ganpat <pganpatchoudhary@gmail.com>
2025-08-28 14:35:26 +0530

    count the frequency of each element in the arraylist and removed dublicate printing

--- ArrayListCollection/Frequency.java
+++ ArrayListCollection/Frequency.java
@@ -6,13 +6,17 @@ import java.util.Arrays;
 public class Frequency {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 10, 40, 30, 20, 30, 20, 10));
+        ArrayList<Integer> visited = new ArrayList<>();
         for (int value : list) {
-            int count = 0;
-            for (int innerValue : list) {
-                if (value == innerValue)
-                    count++;
+            if (!visited.contains(value)) {
+                int count = 0;
+                for (int innerValue : list) {
+                    if (value == innerValue)
+                        count++;
+                }
+                System.out.println(value + " : " + count);
+                visited.add(value);
             }
-            System.out.println(value + " : " + count);
         }
     }
 }

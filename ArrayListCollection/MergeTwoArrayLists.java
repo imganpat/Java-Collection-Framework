aba5d18210dda6dbcb24b5bbbda3864e8e56f97b
Ganpat <pganpatchoudhary@gmail.com>
2025-08-28 11:28:38 +0530

    merge 2 arraylist with dublicates

---
+++ ArrayListCollection/MergeTwoArrayLists.java
@@ -0,0 +1,32 @@
+/* 
+    Merge Two ArrayLists
+        Create two ArrayList<Integer> with some numbers.
+        Merge them into one list.
+        Print the merged list
+ */
+
+package ArrayListCollection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MergeTwoArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(10, 20, 20, 30));
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(30, 40, 50, 60));
+        ArrayList<Integer> mergedList = new ArrayList<>();
+
+        mergedList.addAll(list1);
+        mergedList.addAll(list2);
+
+        display(mergedList);
+
+    }
+
+    public static <T> void display(ArrayList<T> list) {
+        for (T item : list) {
+            System.out.println(item);
+        }
+        System.out.println();
+    }
+}

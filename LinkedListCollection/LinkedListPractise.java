2a7235d65241566886f99994ad8915d79aaf8400
Ganpat <pganpatchoudhary@gmail.com>
2025-08-29 13:13:07 +0530

    Add LinkedList of 5 cities and display them

---
+++ LinkedListCollection/LinkedListPractise.java
@@ -0,0 +1,54 @@
+/* 
+Basic Practice Tasks with LinkedList
+    ✅ Create a LinkedList of Strings
+        ✅ Store names of 5 cities.
+        ✅ Print the LinkedList.
+
+    Access & Modify
+        Print the first and last element.
+        Change the element at index 2.
+        Print the updated LinkedList.
+
+    Add & Remove Elements
+        Add a new city at the beginning.
+        Add a new city at the end.
+        Remove the first and last elements.
+        Print the LinkedList after each change.
+
+    Loop through LinkedList
+        Print all elements using a normal for loop.
+        Then print them again using a for-each loop.
+        Then print using an Iterator.
+
+    Check Size & Search
+        Print the size of the LinkedList.
+        Check if a particular city exists in it.
+ */
+
+package LinkedListCollection;
+
+import java.util.LinkedList;
+
+public class LinkedListPractise {
+    public static void main(String[] args) {
+        // creating a Linked list named cities
+        LinkedList<String> cities = new LinkedList<>();
+
+        // adding 5 city names in the list
+        cities.add("Pune");
+        cities.add("Jaipur");
+        cities.add("Mumbai");
+        cities.add("Bengaluru");
+        cities.add("Ahmedabad");
+
+        // displaying the list
+        display(cities);
+    }
+
+    public static void display(LinkedList<String> list) {
+        for (String element : list) {
+            System.out.println(element);
+        }
+        System.out.println();
+    }
+}

2868f5d3c95dc05e74b6d75ced0271388460d35f
Ganpat <pganpatchoudhary@gmail.com>
2025-08-29 17:45:07 +0530

    checking if a character linked list is palindrome without in-built method

---
+++ LinkedListCollection/CheckPalindrome.java
@@ -0,0 +1,22 @@
+package LinkedListCollection;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        LinkedList<Character> listOfChar = new LinkedList<>(Arrays.asList('R', 'A', 'C', 'E', 'C', 'A', 'R'));
+        int listSize = listOfChar.size();
+        boolean isPalindrome = true;
+
+        for (int i = 0, j = listSize - 1; i < listSize && j >= 0; i++, j--) {
+            if (!listOfChar.get(i).equals(listOfChar.get(j))) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? "True" : "False");
+    }
+
+}

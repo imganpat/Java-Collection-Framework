3ae6fee4b1a655cf7b30ff7abdc9c8bb29c7ae22
Ganpat <pganpatchoudhary@gmail.com>
2025-08-29 17:46:36 +0530

    checking if a character linked list is palindrome using in-built method

--- LinkedListCollection/CheckPalindrome.java
+++ LinkedListCollection/CheckPalindrome.java
@@ -6,17 +6,23 @@ import java.util.LinkedList;
 public class CheckPalindrome {
     public static void main(String[] args) {
         LinkedList<Character> listOfChar = new LinkedList<>(Arrays.asList('R', 'A', 'C', 'E', 'C', 'A', 'R'));
-        int listSize = listOfChar.size();
-        boolean isPalindrome = true;
+        LinkedList<Character> reversedListOfChar = listOfChar.reversed();
 
-        for (int i = 0, j = listSize - 1; i < listSize && j >= 0; i++, j--) {
-            if (!listOfChar.get(i).equals(listOfChar.get(j))) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        System.out.println((listOfChar.equals(reversedListOfChar)) ? "True" : "False");
 
-        System.out.println(isPalindrome ? "True" : "False");
+        // LinkedList<Character> listOfChar = new LinkedList<>(Arrays.asList('R', 'A',
+        // 'C', 'E', 'C', 'A', 'R'));
+        // int listSize = listOfChar.size();
+        // boolean isPalindrome = true;
+
+        // for (int i = 0, j = listSize - 1; i < listSize && j >= 0; i++, j--) {
+        // if (!listOfChar.get(i).equals(listOfChar.get(j))) {
+        // isPalindrome = false;
+        // break;
+        // }
+        // }
+
+        // System.out.println(isPalindrome ? "True" : "False");
     }
 
 }

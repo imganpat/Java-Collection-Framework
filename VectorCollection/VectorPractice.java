ae8cffeb72e5b2f2babc594368bfb3be8c345b82
Ganpat <pganpatchoudhary@gmail.com>
2025-08-30 21:45:02 +0530

    check element is present or not

--- VectorCollection/VectorPractice.java
+++ VectorCollection/VectorPractice.java
@@ -9,9 +9,9 @@ Basic Practice with Vector
     ✅ Insert & Remove
         ✅ Add an element at the beginning and end of the vector.
         ✅ Remove the element at index 3.
-    Check Existence
-        Write a program to check if a given element exists in a Vector<String>.
-        Print "Found" or "Not Found".
+    ✅ Check Existence
+        ✅ Write a program to check if a given element exists in a Vector<String>.
+        ✅ Print "Found" or "Not Found".
 */
 
 package VectorCollection;
@@ -19,39 +19,51 @@ package VectorCollection;
 import java.util.Vector;
 
 public class VectorPractice {
+    // public static void main(String[] args) {
+    // // creating a vector
+    // Vector<Integer> vector = new Vector<>();
+
+    // // adding 5 elements in vector
+    // vector.add(10);
+    // vector.add(20);
+    // vector.add(30);
+    // vector.add(40);
+    // vector.add(50);
+
+    // // printing the vector
+    // System.out.println(vector);
+
+    // // // printing the first element
+    // // System.out.println("First element: " + vector.getFirst());
+
+    // // // printing the last element
+    // // System.out.println("Last element: " + vector.getLast());
+
+    // // // changing the element present at index 2
+    // // vector.set(2, 100);
+    // // System.out.println(vector);
+
+    // // // adding element at the beginning of the vector
+    // // vector.add(0, 100);
+
+    // // // adding element at the end of the vector
+    // // vector.add(200);
+
+    // // // removing the element presebt at index 3
+    // // vector.remove(3);
+
+    // // System.out.println(vector);
+    // }
+
     public static void main(String[] args) {
-        // creating a vector
-        Vector<Integer> vector = new Vector<>();
+        Vector<String> fruits = new Vector<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Papaya");
+        fruits.add("Guava");
+        fruits.add("Grapes");
 
-        // adding 5 elements in vector
-        vector.add(10);
-        vector.add(20);
-        vector.add(30);
-        vector.add(40);
-        vector.add(50);
-
-        // printing the vector
-        System.out.println(vector);
-
-        // // printing the first element
-        // System.out.println("First element: " + vector.getFirst());
-
-        // // printing the last element
-        // System.out.println("Last element: " + vector.getLast());
-
-        // // changing the element present at index 2
-        // vector.set(2, 100);
-        // System.out.println(vector);
-
-        // adding element at the beginning of the vector
-        vector.add(0, 100);
-
-        // adding element at the end of the vector
-        vector.add(200);
-
-        // removing the element presebt at index 3
-        vector.remove(3);
-
-        System.out.println(vector);
+        System.out.println(fruits.contains("Banana") ? "Found" : "Not found");
+        System.out.println(fruits.contains("Mango") ? "Found" : "Not found");
     }
 }
